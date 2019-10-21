43c191e015c77ca1ea55f699cdb7e741e704986c
(Noah) Hanlun Song <hsong@air.org>
2019-10-21 16:52:48 -0400

    Add HTTP monitor & global err handles

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const httpMonitor = require("morgan");
 const apiRoutes = require("./api/index");
 
 const app = express();
+process.env.NODE_ENV === "development" ? app.use(httpMonitor("dev")) : null;
 app.use(
   bodyParser.urlencoded({
     extended: true
@@ -11,12 +13,24 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(apiRoutes);
-// serve static assets normally
+// serves SPA
 app.use(express.static(path.resolve(__dirname, "../client-build")));
-// handle every other route with index.html, which will contain
-// a script tag to your application's JavaScript file(s).
 app.get("*", function(request, response) {
   response.sendFile(path.resolve(__dirname, "../client-build/index.html"));
+});
+// global error handling
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status(404);
+  next(err);
+});
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
 });
 
 module.exports = app;
