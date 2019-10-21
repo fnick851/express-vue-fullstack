1498444f86309234aab98eb301c5a81122ae60da
(Noah) Hanlun Song <hsong@air.org>
2019-10-21 17:17:49 -0400

    Enhanced logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,35 @@
-const express = require("express");
+const fs = require("fs");
+const rfs = require("rotating-file-stream");
 const path = require("path");
+const moment = require("moment");
+const express = require("express");
 const bodyParser = require("body-parser");
-const httpMonitor = require("morgan");
+const morgan = require("morgan");
 const apiRoutes = require("./api/index");
 
 const app = express();
-process.env.NODE_ENV === "development" ? app.use(httpMonitor("dev")) : null;
+
+// http access logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+if (process.env.NODE_ENV === "production") {
+  const logDirectory = path.resolve(__dirname, "../log");
+  // ensure log directory exists
+  fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+  // create a rotating write stream
+  const accessLogStream = rfs(
+    `${moment().format("MMMM Do YYYY, h:mm:ss a")} access.log`,
+    {
+      interval: "1d", // rotate daily
+      path: logDirectory
+    }
+  );
+  // setup the logger
+  app.use(morgan("combined", { stream: accessLogStream }));
+}
+
+// app setup
 app.use(
   bodyParser.urlencoded({
     extended: true
@@ -13,11 +37,13 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(apiRoutes);
+
 // serves SPA
 app.use(express.static(path.resolve(__dirname, "../client-build")));
 app.get("*", function(request, response) {
   response.sendFile(path.resolve(__dirname, "../client-build/index.html"));
 });
+
 // global error handling
 app.use((req, res, next) => {
   const err = new Error("Not Found");
