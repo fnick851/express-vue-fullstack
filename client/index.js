5a83cf4d9e73d05f2b446544e269164fcc606e8b
(Noah) Hanlun Song <hsong@air.org>
2019-10-22 11:09:16 -0400

    Use vue composition api

--- client/index.js
+++ client/index.js
@@ -1,10 +1,14 @@
-import "core-js/stable"
-import "regenerator-runtime/runtime"
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
-import Vue from "vue"
-import App from "./src/App"
+import Vue from "vue";
+import VueCompositionApi from "@vue/composition-api";
+
+import App from "./src/App";
+
+Vue.use(VueCompositionApi);
 
 new Vue({
-    el: "#app",
-    render: h => h(App)
-})
+  el: "#app",
+  render: h => h(App)
+});
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,53 @@
 <template>
+  <div>
+    <h1>Async res: {{state.api_value}}</h1>
+    <hr />
+    <h2>Sum calc</h2>
+    <label for="num-input">Enter numbers separated by comma:</label>
+    <input id="num-input" v-model="state.input_numbers" />
+    <button @click="calculateSum">Calc sum</button>
     <div>
-        <h1>Async res: {{api_value}}</h1>
-        <hr />
-        <h2>Sum calc</h2>
-        <label for="num-input">Enter numbers separated by comma:</label>
-        <input id="num-input" v-model="numbers" />
-        <button @click="calculateSum">Calc sum</button>
-        <div>
-            <h3>Result: {{result}}</h3>
-        </div>
+      <h3>Result: {{state.sum}}</h3>
     </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import { reactive } from "@vue/composition-api";
+import axios from "axios";
 
 export default {
-    name: "App",
-    data() {
-        return {
-            api_value: null,
-            numbers: null,
-            result: 'result will show here'
-        }
-    },
-    async created() {
-        const value = await axios.get('/api/value')
-        this.api_value = value.data
-    },
-    methods: {
-        async calculateSum() {
-            const numArr = this.numbers.split(',').map(n => parseFloat(n))
-            const res = await axios.post('/api/add', {
-                numbers: numArr
-            })
-            this.result = res.data['result']
-        }
+  name: "App",
+  setup() {
+    const state = reactive({
+      sum: "sum result will show here",
+      input_numbers: [],
+      api_value: null
+    });
+
+    (async function() {
+      const value = await axios.get("/api/value");
+      state.api_value = value.data;
+    })();
+
+    async function calculateSum() {
+      const numbers = state.input_numbers.split(",").map(n => parseFloat(n));
+      const res = await axios.post("/api/add", {
+        numbers
+      });
+      state.sum = res.data["result"];
     }
-}
+
+    return {
+      state,
+      calculateSum
+    };
+  }
+};
 </script>
 
 <style scoped>
 h3 {
-    color: blue;
+  color: blue;
 }
 </style>
